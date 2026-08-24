a906dc598208ec16d8eb7d5768eb70dff4a405b5
jajpater <20769148+jajpater@users.noreply.github.com>
2026-08-24 07:18:47 +0000

    Update Bitwarden AppImage

--- package.nix
+++ package.nix
@@ -2,11 +2,11 @@
 
 appimageTools.wrapType2 rec {
   pname = "bitwarden-desktop-appimage";
-  version = "2026.5.0";
+  version = "2026.8.0";
 
   src = fetchurl {
     url = "https://github.com/bitwarden/clients/releases/download/desktop-v${version}/Bitwarden-${version}-x86_64.AppImage";
-    hash = "sha256-QbvzhEZxF1QXu3/9cxW9HdAO+CczZmU2TTQOnSfiO7k=";
+    hash = "sha256-OfoOjqhjaShrUGLiPjYt2ISxBESLoJpITvWKPTevTfo=";
   };
 
   extraPkgs = pkgs: [
